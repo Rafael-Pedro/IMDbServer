a815894a6f1e58a5b3cc082a47dd88e82e3ef4be
Pedro Rafael <pedrorafael1830@gmail.com>
2023-02-14 14:10:49 -0300

    Adicionadno crud no userRepository

--- IMDb.Server.Infra.Database.Abstraction/Respositories/IUsersRepository.cs
+++ IMDb.Server.Infra.Database.Abstraction/Respositories/IUsersRepository.cs
@@ -1,5 +1,11 @@
-﻿namespace IMDb.Server.Infra.Database.Abstraction.Respositories;
+﻿using IMDb.Server.Domain.Entities;
+
+namespace IMDb.Server.Infra.Database.Abstraction.Respositories;
 
 public interface IUsersRepository
 {
+    Task Create(Users users, CancellationToken cancellationToken);
+    void Delete(Users users);
+    void Update(Users users);
+    Task<bool> IsUniqueEmail(string email, CancellationToken cancellationToken);
 }
