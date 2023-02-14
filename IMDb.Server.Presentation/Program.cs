df81ee16e20e90d175db128626b0e10596932c02
Pedro Rafael <pedrorafael1830@gmail.com>
2023-02-14 14:10:23 -0300

    Adicionando pacote e dependencia na program

--- IMDb.Server.Presentation/Program.cs
+++ IMDb.Server.Presentation/Program.cs
@@ -1,3 +1,5 @@
+using IMDb.Server.Infra.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDatabase();
 
 var app = builder.Build();
 
