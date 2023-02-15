4fc91e6877c3956369f3097a0019fc4ba0a521d2
Pedro Rafael <pedrorafael1830@gmail.com>
2023-02-15 14:09:05 -0300

    Adicionando alguns repositorios

--- IMDb.Server.Infra.Database.Abstraction/Respositories/ICastRepository.cs
+++ IMDb.Server.Infra.Database.Abstraction/Respositories/ICastRepository.cs
@@ -1,5 +1,15 @@
-﻿namespace IMDb.Server.Infra.Database.Abstraction.Respositories;
+﻿using IMDb.Server.Domain.Entities;
+
+namespace IMDb.Server.Infra.Database.Abstraction.Respositories;
 
 public interface ICastRepository
 {
+    Task Create(Cast cast, CancellationToken cancellationToken);
+    void Delete(Cast cast);
+    void Update(Cast cast);
+    Task<Cast?> GetById(int id);
+    Task<Cast?> GetByName(string name, CancellationToken cancellationToken);
+    Task<Cast?> GetByBirthDate(string BirhDate, CancellationToken cancellationToken);
+    Task<CastActMovies?> GetByMovie(int id, CancellationToken cancellationToken);
+    Task<bool> IsAlreadyRegistered(string name, CancellationToken cancellationToken);
 }
