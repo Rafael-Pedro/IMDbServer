a90c605c5f9193ff19b70ce29b151a22c444fe21
Pedro Rafael <pedrorafael1830@gmail.com>
2023-02-23 14:41:08 -0300

    Adicionando SetUserInfoBehaviour

---
+++ IMDb.Server.Application/Behaviours/SetUserInfoBehavior.cs
@@ -0,0 +1,22 @@
+﻿using FluentResults;
+using IMDb.Server.Application.UserInfo;
+using MediatR;
+
+namespace IMDb.Server.Application.Behaviours;
+
+public class SetUserInfoBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : ResultBase, new()
+    where TResponse : IRequest<TRequest>
+{
+    private readonly IUserInfo userInfo;
+
+    public SetUserInfoBehavior(IUserInfo userInfo)
+    {
+        this.userInfo = userInfo;
+    }
+
+    public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
