290d54239a39ecd9d634941fb1988c496502e32a
Pedro Rafael <pedrorafael1830@gmail.com>
2023-02-23 14:41:56 -0300

    Adicionando Criptografia

---
+++ IMDb.Server.Application/Services/Cryptography/CryptographyService.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace MulviParking.Server.Application.Services.Cryptography;
+public class CryptographyService : ICryptographyService
+{
+    public bool Compare(byte[] originalHash, byte[] originalSalt, string plainText)
+    {
+        var plainTextHash = Hash(plainText, originalSalt);
+        return originalHash.SequenceEqual(plainTextHash);
+    }
+
+    public byte[] CreateSalt()
+    {
+        var rng = RandomNumberGenerator.Create();
+        var buff = new byte[10];
+        rng.GetBytes(buff);
+        return buff;
+    }
+
+    public byte[] Hash(string plainText, byte[] salt)
+        => SHA256.HashData(Encoding.UTF8.GetBytes(plainText).Concat(salt).ToArray());
+}
---
+++ IMDb.Server.Application/Services/Cryptography/ICryptographyService.cs
@@ -0,0 +1,7 @@
+﻿namespace MulviParking.Server.Application.Services.Cryptography;
+public interface ICryptographyService
+{
+    byte[] Hash(string plainText, byte[] salt);
+    byte[] CreateSalt();
+    bool Compare(byte[] originalHash, byte[] originalSalt, string plainText);
+}
