f4cde9f5527cf69a32fcb3fd66762b6bc47c61ac
Pedro Rafael <pedrorafael1830@gmail.com>
2023-02-27 14:24:44 -0300

    Adicionando adminRepository e implementando GenresRepository

--- IMDb.Server.Infra.Database/Repositories/GenresRepository.cs
+++ IMDb.Server.Infra.Database/Repositories/GenresRepository.cs
@@ -1,7 +1,27 @@
-﻿using IMDb.Server.Infra.Database.Abstraction.Respositories;
+﻿using IMDb.Server.Domain.Entities;
+using IMDb.Server.Infra.Database.Abstraction.Respositories;
 
 namespace IMDb.Server.Infra.Database.Repositories;
 
 public class GenresRepository : IGenresRepository
 {
+    public Task<Genres> Create(Genres genres, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(Genres genres)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Genres> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Genres genres)
+    {
+        throw new NotImplementedException();
+    }
 }
