f425d06516b64a5c4e6a876ecdf98859a6c41266
Pedro Rafael <pedrorafael1830@gmail.com>
2023-02-27 14:14:25 -0300

    Iniciando dependecyInjection

---
+++ IMDb.Server.Application/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using IMDb.Server.Application.Behaviours;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IMDb.Server.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApp(this IServiceCollection services)
+    {
+        services.AddMediatR(AppDomain.CurrentDomain.GetAssemblies());
+
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ErrorHandlingBehaviour<,>));
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationHandlingBehaviour<,>));
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(SetUserInfoBehaviour<,>));
+
+
+
+        return services;
+    }
+}
