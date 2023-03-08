958a08c555aa1deee83e12a13844db878761d5a3
Pedro Rafael <pedrorafael1830@gmail.com>
2023-03-08 14:25:07 -0300

    Iniciando feature de disable de admin

---
+++ IMDb.Server.Application/Features/Account/Adm/Disable/DisableAccountAdmCommand.cs
@@ -0,0 +1,10 @@
+﻿using FluentResults;
+using IMDb.Server.Application.UserInfo;
+using MediatR;
+
+namespace IMDb.Server.Application.Features.Account.Adm.Disable;
+
+public record DisableAccountAdmCommand() : IRequest<Result>, IUserId
+{
+    public int UserId { get; set; }
+}
